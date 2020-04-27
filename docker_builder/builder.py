65dc0aad5efeb0f91ef881fc48dea834cc0175e5
javier162380 <javierllorente16@gmail.com>
2020-04-27 17:06:33 +0200

    adding docker builder module and change enviroment

---
+++ docker_builder/builder.py
@@ -0,0 +1,42 @@
+import os
+
+import docker
+
+
+class Builder:
+
+    def __init__(self, build_instance):
+
+        self.build_instance = build_instance
+        self.docker_instance = docker.APIClient()
+
+    @property
+    def format_image_name(self):
+        return f"{self.build_instance.image_name}/{':'.join(self.build_instance.tags)}"
+
+    @property
+    def dockerfile_path(self):
+        path, dockerfile = os.path.split(self.build_instance.dockerfile)
+
+        if dockerfile == '':
+            return './'
+
+        return path
+
+    @property
+    def dockerfile_name(self):
+        path, dockerfile = os.path.split(self.build_instance.dockerfile)
+
+        return dockerfile
+
+    def build_image(self):
+        return [output for output in self.docker_instance.build(
+            path=self.dockerfile_path,
+            dockerfile=self.dockerfile_name,
+            tag=self.format_image_name,
+            buildargs=self.build_instance.args if self.build_instance.args else {}
+        )]
+
+    def execute(self):
+
+        self.build_image()
