f99365a735d2cf5b59f76bb789b75d83cb521b30
javier162380 <javierllorente16@gmail.com>
2020-04-27 17:03:38 +0200

    change docker builder endpoints

--- docker_builder/endpoints/build.py
+++ docker_builder/endpoints/build.py
@@ -3,17 +3,27 @@ import time
 from rq import Queue
 
 sys.path.append('..')
-from settings import Q, BUILD_TIMEOUT, FAILED_JOB_TIMEOUT, SUCCESSFUL_JOB_TIMEOUT
+from builder import Builder
+from models import Build, BuildResponse
 from worker import conn
-from models import Build
+from settings import Q, BUILD_TIMEOUT, FAILED_JOB_TIMEOUT, SUCCESSFUL_JOB_TIMEOUT
 
 
-def sleep():
-    return time.sleep(10)
+def build_submit(build: Build):
 
+    build_instance = Build(build_instance=build)
+    build_instance.execute()
 
 async def build_docker(build: Build):
+
+    builder = Builder(build_instance=build)
+    
     result = Q.enqueue(
-        sleep, 10, job_timeout=BUILD_TIEMOUT, failure_ttl=FAILED_JOB_TIEMOUT,
-        result_ttl=SUCCESSFUL_JOB_TIMEOUT)
-    return result
+        build_submit,
+        build,
+        job_timeout=BUILD_TIMEOUT,
+        result_ttl=SUCCESSFUL_JOB_TIMEOUT,
+        failure_ttl=FAILED_JOB_TIMEOUT
+    )
+
+    return BuildResponse(build_id=result.id)
--- docker_builder/endpoints/status.py
+++ docker_builder/endpoints/status.py
@@ -1,8 +1,18 @@
+from worker import conn
+from models import Status
 import sys
+from rq.job import Job
+import rq.exceptions
 
 sys.path.append('..')
-from models import Status
 
 
-async def check_status(build_id: int):
-    return Status(build_id=build_id, build_status='in_progress')
+async def check_status(build_id: str):
+
+    try:
+        job = Job.fetch(build_id, connection=conn)
+        status = job.get_status()
+        return Status(build_id=build_id, build_status=status)
+
+    except rq.exceptions.NoSuchJobError:
+        return Status(build_id=build_id, build_status="NotFound/Deleted")
