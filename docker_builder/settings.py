778e0da09b9396fdf156c175620102227c6948ae
javier162380 <javierllorente16@gmail.com>
2020-04-27 17:05:46 +0200

    adding settings

--- docker_builder/settings.py
+++ docker_builder/settings.py
@@ -1,7 +1,9 @@
 import os
 from rq import Queue
+from rq.job import Job
+from worker import conn
 
-Q = Queue(connection=os.getenv('REDIS_URL'))
-BUILD_TIMEOUT = 600
+Q = Queue(connection=conn)
+BUILD_TIMEOUT = 1200
 SUCCESSFUL_JOB_TIMEOUT = 36000
 FAILED_JOB_TIMEOUT = 1314000
