f27f5dd0a61016c30c4283b6d81a421c41a4bc82
javier162380 <javierllorente16@gmail.com>
2020-04-27 17:05:19 +0200

    adding response router

--- docker_builder/router/router.py
+++ docker_builder/router/router.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 
 sys.path.append('..')
 from endpoints import build, health, status
+from models import Status, BuildResponse
 
 
 router = APIRouter()
 
-router.add_api_route('/v1/build', endpoint=build.build_docker, methods=["POST"])
+router.add_api_route('/v1/build', endpoint=build.build_docker, methods=["POST"], response_model=BuildResponse)
 router.add_api_route('/v1/health', endpoint=health.health, methods=["GET"])
-router.add_api_route('/v1/status', endpoint=status.check_status, methods=["GET"])
+router.add_api_route('/v1/status', endpoint=status.check_status, methods=["GET"], response_model=Status)
